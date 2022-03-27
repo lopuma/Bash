01bf360d35797cd57729cfa4a4053699ec652fdf
lopuma <lopumacp@hotmail.com>
2022-03-28 00:18:12 +0200

    28/03/2022

--- sudo_itss_preg4.sh
+++ sudo_itss_preg4.sh
@@ -11,10 +11,10 @@ LISTA=(
     # done
 function ZY.1.2.3(){
     value=$(ls -la /etc/profile.d/secondary_logging_IBM.sh 2>&1)
-    if [[ "$value" == *"No such"* ]]; then
+    if [[ "$value" == *"No such"* || "$value" == *"No existe"* ]]; then
         resultado_ZY123="NO"
-        message_ZY123="No existe el fichero de secondary_loggin"
-        echo -e "Checking for \e[33mZY.1.2.3 Logging ....\e[0m $resultado_ZY123 : '$message_ZY123'"
+        message_ZY123="$value"
+        echo -e "Checking for \e[31mZY.1.2.3 Logging ....\e[0m $resultado_ZY123 : '$message_ZY123'"
     else
         resultado_ZY123="YES"
         message_ZY123=""
@@ -23,10 +23,10 @@ function ZY.1.2.3(){
 }
 function ZY.1.2.4(){
     value=$(ls -la /etc/logrotate.conf 2>&1)
-    if [[ "$value" == *"No such"* ]]; then
+    if [[ "$value" == *"No such"* || "$value" == *"No existe"* ]]; then
         resultado_ZY124="NO"
-        message_ZY124="No existe el fichero de /etc/logrotate.conf"
-        echo -e "Checking for \e[33mZY.1.2.4 Logging ....\e[0m $resultado_ZY124 : '$message_ZY124'"
+        message_ZY124="$value"
+        echo -e "Checking for \e[31mZY.1.2.4 Logging ....\e[0m $resultado_ZY124 : '$message_ZY124'"
     else
         resultado_ZY124="YES"
         message_ZY124=""
@@ -35,10 +35,10 @@ function ZY.1.2.4(){
 }
 function ZY.1.2.5(){
     value=$(ls -la /etc/sudoers 2>&1)
-    if [[ "$value" == *"No such"* ]]; then
+    if [[ "$value" == *"No such"* || "$value" == *"No existe"* ]]; then
         resultado_ZY125="NO"
-        message_ZY125="No existe el fichero de /etc/sudoers"
-        echo -e "Checking for \e[33mZY.1.4.5 System Settings ....\e[0m $resultado_ZY125 : '$message_ZY125'"
+        message_ZY125="$value"
+        echo -e "Checking for \e[31mZY.1.4.5 System Settings ....\e[0m $resultado_ZY125 : '$message_ZY125'"
     else
         resultado_ZY125="YES"
         message_ZY125=""
@@ -46,6 +46,7 @@ function ZY.1.2.5(){
     fi
 }
 clear
+echo "servidor : " $HOSTNAME
 echo -e "\e[32m----------------------------INICIO----------------------------\e[0m"
 sleep 0.5s    
 ZY.1.2.3
@@ -53,7 +54,10 @@ sleep 0.5s
 ZY.1.2.4
 sleep 0.5s    
 ZY.1.2.5
-echo -e "\e[31m-----------------------------FIN------------------------------\e[0m"
+echo -e "\e[32m-----------------------------FIN------------------------------\e[0m"
+sleep 0.5
+#rm -r /home/$(logname)/.ssh/authorized_keys
+
 # for var in "${LISTA[@]}"
 # do
 #     sleep 0.5s    
